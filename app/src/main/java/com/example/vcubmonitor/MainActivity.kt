6bbcb2c665a5553e9cefd4787c87891c08639a8c
Akram Abdoul-Wahab <akram.abdl@outlook.fr>
2022-11-25 11:33:22 +0100

    init

---
+++ app/src/main/java/com/example/vcubmonitor/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.vcubmonitor
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
