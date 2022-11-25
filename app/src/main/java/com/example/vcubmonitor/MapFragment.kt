8fdf39e9c84fc128332aadc213e36aa67bc75442
yohannmarchand <marchandyohannnnn@gmail.com>
2022-11-25 14:32:52 +0100

    integrate google map in the view

--- app/src/main/java/com/example/vcubmonitor/MapFragment.kt
+++ app/src/main/java/com/example/vcubmonitor/MapFragment.kt
@@ -6,6 +6,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
@@ -17,7 +23,32 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class MapFragment : Fragment() {
+    private lateinit var lat: String
+    private lateinit var lon: String
+    private lateinit var name: String
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        lat = "44.83784"
+        lon = "-0.59028"
+        name = "Bordeaux"
+    }
+
+    private val callback = OnMapReadyCallback { googleMap ->
+        /**
+         * Manipulates the map once available.
+         * This callback is triggered when the map is ready to be used.
+         * This is where we can add markers or lines, add listeners or move the camera.
+         * In this case, we just add a marker near Sydney, Australia.
+         * If Google Play services is not installed on the device, the user will be prompted to
+         * install it inside the SupportMapFragment. This method will only be triggered once the
+         * user has installed Google Play services and returned to the app.
+         */
+        val latLng = LatLng(lat.toDouble(), lon.toDouble())
+        googleMap.addMarker(MarkerOptions().position(latLng).title(getString(R.string.maps_marker_title, name)))
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 11f))
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -27,5 +58,9 @@ class MapFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_map, container, false)
     }
 
-
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
+        mapFragment?.getMapAsync(callback)
+    }
 }
