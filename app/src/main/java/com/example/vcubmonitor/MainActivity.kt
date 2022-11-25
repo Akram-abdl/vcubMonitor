0d1f24bbbdb5a770523e16452ddfe2a1028b67f2
Akram Abdoul-Wahab <akram.abdl@outlook.fr>
2022-11-25 12:54:15 +0100

    added bottom nav bar

--- app/src/main/java/com/example/vcubmonitor/MainActivity.kt
+++ app/src/main/java/com/example/vcubmonitor/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.vcubmonitor
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.vcubmonitor.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        navController=Navigation.findNavController(this,R.id.activity_main_nav_host_fragment)
+        setupWithNavController(binding.bottomNavigationView,navController)
+
+
+
     }
 }
