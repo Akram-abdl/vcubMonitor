d6a6d117fefdee75ffff8569d287df9d4965958b
Akram Abdoul-Wahab <akram.abdl@outlook.fr>
2022-12-01 17:02:05 +0100

    stashing advancement

--- app/src/main/java/com/example/vcubmonitor/ListFragment.kt
+++ app/src/main/java/com/example/vcubmonitor/ListFragment.kt
@@ -5,30 +5,47 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ListView
+import android.widget.Toast
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+//private const val ARG_PARAM1 = "param1"
+//private const val ARG_PARAM2 = "param2"
+
 
 /**
  * A simple [Fragment] subclass.
  * Use the [ListFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class ListFragment : Fragment() {
 
+class ListFragment : Fragment() {
+    private lateinit var listViewData: ListView
 //    private var param1: String? = null
 //    private var param2: String? = null
-
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+
+
+        Toast.makeText(context, "test", Toast.LENGTH_SHORT).show()
         return inflater.inflate(R.layout.fragment_list, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        listViewData = view.findViewById(R.id.listViewData)
+
+
+        
+
+
+
     }
 
 
 }
+
